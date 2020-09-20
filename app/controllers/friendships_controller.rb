7d4afcc6cdb2c2d735ec99382fc1d874a4cc225f
Cristian Deleon <libaration@gmail.com>
2020-09-20 04:31:04 -0400

    Prevent adding friend duplication

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,11 +3,12 @@ class FriendshipsController < ApplicationController
   get '/user/:id/add' do
     if current_user == User.find(params[:id])
       "You can't add yourself..."
-    elsif current_user != User.find(params[:id]) && logged_in?
+      binding.pry
+    elsif current_user != User.find(params[:id]) && logged_in? && !current_user.friends.include?(User.find(params[:id]))
       @newfriend = User.find(params[:id])
       current_user.friends << @newfriend
       @newfriend.friends << current_user
-      redirect "/user/#{@newfriend}"
+      redirect "/user/#{@newfriend.id}"
     end
   end
 
