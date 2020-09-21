9f18bc3bdcf5121ba7e3c14e13d43e8c9b055219
Cristian Deleon <libaration@gmail.com>
2020-09-21 19:57:57 -0400

    Allow users to accept/deny friend requests. Prevent duplication by checking if user is already in friends pending_friends list before adding themselves into it.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,9 +6,22 @@ class FriendshipsController < ApplicationController
       "You can't add yourself..."
     elsif current_user != User.find(params[:id]) && logged_in? && !current_user.friends.include?(User.find(params[:id]))
       @newfriend = User.find(params[:id])
-      @newfriend.pending_friends << current_user
+      @newfriend.pending_friends << current_user unless @newfriend.pending_friends.include?(current_user)
       redirect "/user/#{@newfriend.id}"
     end
+  end
+
+  post '/user/:id/approve' do
+    @user = User.find(params[:id])
+    @friend = User.find(params[:friend_id])
+    if logged_in? && current_user == @user && current_user.pending_friends.include?(@friend)
+      @user.pending_friends.delete(@friend)
+      @user.friends << @friend
+      @friend.friends << @user
+    else
+      'An error has occurred'
+    end
+    redirect "/user/#{current_user.id}/friend_requests"
   end
 
 
