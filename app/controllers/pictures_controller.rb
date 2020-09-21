bff4da12c7c823927605b98c74088c4f5aa5b1a0
Cristian Deleon <libaration@gmail.com>
2020-09-21 02:03:28 -0400

    Add Rack::MethodOverride, Allow user to destroy uploaded pictures.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,6 +10,17 @@ class PicturesController < ApplicationController
     erb :'user/pictures/show', :layout => :template
   end
 
+  delete '/user/:id/pictures/:picture_id' do
+    @user = User.find(params[:id])
+    @image = Images.find(params[:picture_id])
+    if @user == current_user
+      @image.destroy
+      redirect "/user/#{@user.id}/pictures"
+    else
+      'You do not have permission to perform this action'
+    end
+  end
+
   post '/user/:id/pictures' do
     img = Images.new
     img.image  = params[:file] #carrierwave uploads using params here
