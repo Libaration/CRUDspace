cc789215b0cc20e57543d0f5e1273162a3b924f3
Cristian Deleon <libaration@gmail.com>
2020-09-21 02:28:27 -0400

    Allow users to patch/edit photo captions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,7 +13,7 @@ class PicturesController < ApplicationController
   delete '/user/:id/pictures/:picture_id' do
     @user = User.find(params[:id])
     @image = Images.find(params[:picture_id])
-    if @user == current_user
+    if logged_in? && @user == current_user
       @image.destroy
       redirect "/user/#{@user.id}/pictures"
     else
@@ -22,11 +22,23 @@ class PicturesController < ApplicationController
   end
 
   post '/user/:id/pictures' do
-    img = Images.new
-    img.image  = params[:file] #carrierwave uploads using params here
-    img.user = current_user
-    img.caption = params[:caption]
-    img.save!
-    redirect "/user/#{current_user.id}/pictures/#{img.id}"
+    if logged_in? && current_user == User.find(params[:id])
+      img = Images.new
+      img.image  = params[:file] #carrierwave uploads using params here
+      img.user = current_user
+      img.caption = params[:caption]
+      img.save!
+      redirect "/user/#{current_user.id}/pictures/#{img.id}"
+    else
+      'You do not have permission to perform this action'
+    end
+  end
+
+  patch '/user/:id/pictures/:picture_id' do
+    if logged_in? && current_user == User.find(params[:id])
+    img = Images.find(params[:picture_id])
+    img.update(caption: params[:caption])
+    redirect "/user/#{current_user.id}/pictures"
+    end
   end
 end
