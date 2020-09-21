21e44476ca95c22f09f40444999e3363be9bb069
Cristian Deleon <libaration@gmail.com>
2020-09-21 11:45:44 -0400

    Edit User model to only allow relation if conditional is met

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :images, class_name: 'Images'
-  has_many :friendships
+  has_many :friendships, -> { where(accepted: true)}
   has_many :friends, through: :friendships
 end
