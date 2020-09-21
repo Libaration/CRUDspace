d00698431bfc3a4397d5a220cb17b369ba8eac61
Cristian Deleon <libaration@gmail.com>
2020-09-21 10:52:49 -0400

    Will allow users to destroy the assoication between then when removing friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,4 +12,17 @@ class FriendshipsController < ApplicationController
     end
   end
 
+
+  get '/user/:id/remove' do
+    redirect '/login' if !logged_in?
+    if current_user == User.find(params[:id])
+      "You can't add yourself..."
+    elsif current_user != User.find(params[:id]) && logged_in? && current_user.friends.include?(User.find(params[:id]))
+      @friend = User.find(params[:id])
+      current_user.friends.destroy(@friend)
+      @friend.friends.destroy(current_user)
+      redirect "/user/#{@friend.id}"
+    end
+  end
+
 end
