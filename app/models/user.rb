ddcf6808d9233636a10775cef0152259337f3032
Cristian Deleon <libaration@gmail.com>
2020-09-21 11:56:35 -0400

    (WIP) Allow users to have pending friends through jointable conditional

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :images, class_name: 'Images'
   has_many :friendships, -> { where(accepted: true)}
+  has_many :pending_friendships, -> { where(accepted: false)}, class_name: Friendship
   has_many :friends, through: :friendships
+  has_many :pending_friends, through: :pending_friendships, :source => :user
 end
