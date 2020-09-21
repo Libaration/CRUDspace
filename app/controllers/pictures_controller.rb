a535c6d5ea1eca8ddeb76ef8260c396c98203881
Cristian Deleon <libaration@gmail.com>
2020-09-21 01:54:50 -0400

    Allow users to upload new photos and associate those photos with themselves.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,4 +9,12 @@ class PicturesController < ApplicationController
     @image = Images.find(params[:picture_id])
     erb :'user/pictures/show', :layout => :template
   end
+
+  post '/user/:id/pictures' do
+    img = Images.new
+    img.image  = params[:file] #carrierwave uploads using params here
+    img.user = current_user
+    img.save!
+    redirect "/user/#{current_user.id}/pictures/#{img.id}"
+  end
 end
