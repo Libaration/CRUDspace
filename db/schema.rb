248c34e0c660b24b0731576d757d8ed80f0d3487
Cristian Deleon <libaration@gmail.com>
2020-09-21 23:34:19 -0400

    Bugfix timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200922024603) do
+ActiveRecord::Schema.define(version: 20200922033235) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
@@ -29,7 +29,8 @@ ActiveRecord::Schema.define(version: 20200922024603) do
     t.string   "content"
     t.integer  "reciever_id"
     t.integer  "sender_id"
-    t.datetime "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
