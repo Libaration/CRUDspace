d63bf7e8a4c03ee980ef963a0d05a41a14a6ac99
Cristian Deleon <libaration@gmail.com>
2020-09-22 01:49:46 -0400

    Validate if User is the receiver of message before showing to prevent reading other users messages.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,9 @@
 class MessagesController < ApplicationController
   get '/user/:id/messages/:message_id' do
     @user = User.find(params[:id])
-    @message = User.find(params[:id]).messages.where(id: params[:message_id]).first
-    if logged_in? && @user == current_user
+    @message = Message.find(params[:message_id])
+    if logged_in? && @user == current_user && @message.receiver == current_user
+      # @message = User.find(params[:id]).messages.where(id: params[:message_id]).first
       erb :'/user/messages/show', layout: :template
     else
       redirect '/login'
