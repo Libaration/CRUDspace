bd1dab9980f326d1d8d65401d9dd52f6e2450d99
Cristian Deleon <libaration@gmail.com>
2020-09-22 02:00:11 -0400

    Add read column to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200922041451) do
+ActiveRecord::Schema.define(version: 20200922055820) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20200922041451) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "subject"
+    t.boolean  "read",        default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
