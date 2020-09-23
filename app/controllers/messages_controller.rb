2a8692dee087b7e156cf4cf6f8954af1f1f9e335
Cristian Deleon <libaration@gmail.com>
2020-09-22 21:22:07 -0400

    Lets users message each other. Will gsub linebreaks to allow proper message formatting. Allows users to see they have replied.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,32 @@
 class MessagesController < ApplicationController
+
+  post'/user/:id/messages' do
+    @user = User.find(params[:id])
+    @message = Message.find(params[:msg_id])
+    if logged_in? && @user == current_user && @message.receiver == current_user
+      params[:content].gsub!(/\r\n/, '<br />')
+      Message.create(params.except(:msg_id, :id)).tap do |new_message|
+        new_message.sender = @user
+        new_message.receiver = @message.sender
+        new_message.save
+      end
+      @message.update(replied: true)
+      redirect "/user/#{@user.id}/messages"
+    else
+      redirect '/login'
+    end
+  end
+
+  get'/user/:id/messages/:msg_id/reply' do
+    @user = User.find(params[:id])
+    @message = Message.find(params[:msg_id])
+    if logged_in? && @user == current_user && @message.receiver == current_user
+      erb :'user/messages/new', layout: :template
+    else
+      redirect '/login'
+    end
+  end
+
   get '/user/:id/messages/:message_id' do
     @user = User.find(params[:id])
     @message = Message.find(params[:message_id])
