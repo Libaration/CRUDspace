648b5f07508e93e1b3e945572849044284971c10
Cristian Deleon <libaration@gmail.com>
2020-09-23 15:47:24 -0400

    Matches oldschool layout CSS to main css to allow copy paste layouts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,7 +76,12 @@ class UsersController < ApplicationController
     if logged_in? && @user == current_user
       @user.update(params.except(:css))
       path = "./app/public/profile_css/#{@user.id}_custom_css.css"
-      content = Sanitize::CSS.stylesheet(params[:css], Sanitize::Config::RELAXED)
+      content = Sanitize::CSS.stylesheet(params[:css], Sanitize::Config::RELAXED).gsub(".module", ".topRight, .topLeft").gsub("div.contentTop", "div.extended") + "
+.topRight{
+    float: right;
+    width: calc( 60% - 20px );
+     padding: 0px
+}"
       File.open(path, "w+") do |f|
         f.write(content)
       end
