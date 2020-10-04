cd18ec773db2ba42c9ed2d53712140cab706be0c
Cristian Deleon <libaration@gmail.com>
2020-10-03 23:31:49 -0400

    Allows uploading of profile songs in songs controller.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,17 @@ class SongsController < ApplicationController
       erb :'/users/profile_song/new', :layout => :template
     end
   end
+
+  post '/users/:id/profile_song' do
+    if logged_in? && (@user = User.find_by_slug(params[:id]) || User.find(params[:id])) && current_user == @user
+      if params[:file][:type].include?("mpeg")
+        @user.song.destroy if !@user.song.nil?
+        song = Song.new
+        song.profilesong  = params[:file] #carrierwave uploads using params here
+        song.user = @user
+        song.save!
+        redirect "/users/#{@user.url.nil? ? @user.id : @user.url}"
+      end
+    end
+  end
 end
